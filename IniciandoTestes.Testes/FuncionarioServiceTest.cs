b18f355a17e94d25bc5ec1f942aa31e4c05b44b2
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-24 11:49:29 -0300

    cobrindo 81% do fluxo

--- IniciandoTestes.Testes/FuncionarioServiceTest.cs
+++ IniciandoTestes.Testes/FuncionarioServiceTest.cs
@@ -4,6 +4,7 @@ using System;
 using Xunit;
 using IniciandoTestes.Entidades;
 using IniciandoTestes.Servicos;
+using Xunit.Sdk;
 
 namespace IniciandoTestes.Tests
 {
@@ -15,6 +16,9 @@ namespace IniciandoTestes.Tests
         {
             _faker = new Faker();
         }
+
+//teste de sucesso com dados ok
+
         [Theory]
         [MemberData(nameof(GetFuncionariosValidos))]
         public void AdicionarFuncionario_DeveConcluir_QuandoDadosValidos(Funcionario funcionario)
@@ -25,6 +29,18 @@ namespace IniciandoTestes.Tests
             // Act & Assert
             sut.AdicionarFuncionario(funcionario);
         }
+
+        [Theory]
+        [MemberData(nameof(GetFuncionariosInvalidos))]
+        public void AdicionarFuncionario_DeveLancarExcecao_QuandoDadosInvalidos(Funcionario funcionario, Type excecaoEsperada)
+        {
+            // Arrange
+            var sut = new FuncionarioService();
+
+            // Act & Assert
+            Assert.Throws(excecaoEsperada, () => sut.AdicionarFuncionario(funcionario));
+        }
+
         public static IEnumerable<object[]> GetFuncionariosValidos()
         {
             var faker = new Faker();
@@ -46,7 +62,7 @@ namespace IniciandoTestes.Tests
             new Funcionario
             {
                 Nome = faker.Name.FullName(),
-Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
                 Senioridade = Senioridade.Pleno,
                 Salario = faker.Random.Double(5500, 8000)
             }
@@ -62,8 +78,74 @@ Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(
                 Salario = faker.Random.Double(8000, 20000)
             }
             };
+        }
 
+        public static IEnumerable<object[]> GetFuncionariosInvalidos()
+        {
+            var faker = new Faker();
 
+            // Nome inválido
+            yield return new object[]
+            {
+            new Funcionario
+            {
+                Nome = "Jo",
+                Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                Senioridade = Senioridade.Junior,
+                Salario = 4000
+            },
+            typeof(FormatException)
+            };
+
+            // Idade fora do permitido
+            yield return new object[]
+            {
+            new Funcionario
+            {
+                Nome = faker.Name.FullName(),
+                                Nascimento = faker.Date.Past(60, DateTime.Now.AddYears(-60)), // Idade inválida
+                Senioridade = Senioridade.Pleno,
+                Salario = 6000
+            },
+            typeof(Exception)
+            };
+
+            // Salário fora do intervalo para cada senioridade
+            yield return new object[]
+            {
+            new Funcionario
+            {
+                Nome = faker.Name.FullName(),
+                Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                Senioridade = Senioridade.Junior,
+                Salario = faker.Random.Double(0, 3199) // Salário inválido para Junior
+            },
+            typeof(Exception)
+            };
+
+            yield return new object[]
+            {
+            new Funcionario
+            {
+                Nome = faker.Name.FullName(),
+                Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                Senioridade = Senioridade.Pleno,
+                Salario = faker.Random.Double(0, 5499) // Salário inválido para Pleno
+            },
+            typeof(Exception)
+            };
+
+            yield return new object[]
+            {
+            new Funcionario
+            {
+                Nome = faker.Name.FullName(),
+                Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                Senioridade = Senioridade.Senior,
+                Salario = faker.Random.Double(0, 7999) // Salário inválido para Senior
+            },
+            typeof(Exception)
+            };
         }
     }
 }
