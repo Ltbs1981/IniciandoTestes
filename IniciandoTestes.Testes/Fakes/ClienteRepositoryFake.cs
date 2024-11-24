e60c364262bbf0e798a8abf32b66f8b95fb8750a
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-24 10:58:48 -0300

    testes de sucesso

--- IniciandoTestes.Testes/Fakes/ClienteRepositoryFake.cs
+++ IniciandoTestes.Testes/Fakes/ClienteRepositoryFake.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using IniciandoTestes.Contratos;
+using IniciandoTestes.Entidades;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace IniciandoTestes.Testes.Fakes
+namespace IniciandoTestes.Tests.Fakes
 {
-    internal class ClienteRepositoryFake
+    internal class ClienteRepositoryFake : IClienteRepository
     {
+        public void AddCliente(Cliente cliente)
+        {
+
+        }
+
+        public List<Cliente> GetAll()
+        {
+            return new List<Cliente>();
+        }
+
+        public Cliente GetCliente(Guid id)
+        {
+            var idFake = new Guid("12ACA6F1-EECF-4CBA-A299-081FFF88EDE5");
+            if (id == idFake)
+                return new Cliente()
+                {
+                    Nome = "Arthur",
+                    Nascimento = new DateTime(2003, 04, 12),
+                    Id = idFake
+                };
+
+            return null;
+        }
+
+        public Cliente GetCliente(string nome)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- IniciandoTestes.Testes/FuncionarioServiceTest.cs
+++ IniciandoTestes.Testes/FuncionarioServiceTest.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using Bogus;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using Xunit;
+using IniciandoTestes.Entidades;
+using IniciandoTestes.Servicos;
 
-namespace IniciandoTestes.Testes
+namespace IniciandoTestes.Tests
 {
-    internal class FuncionarioServiceTest
+    public class FuncionarioServiceTest
     {
+        private readonly Faker _faker;
+
+        public FuncionarioServiceTest()
+        {
+            _faker = new Faker();
+        }
+        [Theory]
+        [MemberData(nameof(GetFuncionariosValidos))]
+        public void AdicionarFuncionario_DeveConcluir_QuandoDadosValidos(Funcionario funcionario)
+        {
+            // Arrange
+            var sut = new FuncionarioService();
+
+            // Act & Assert
+            sut.AdicionarFuncionario(funcionario);
+        }
+        public static IEnumerable<object[]> GetFuncionariosValidos()
+        {
+            var faker = new Faker();
+
+            // Funcionários com salários válidos para cada nível de senioridade
+            yield return new object[]
+            {
+            new Funcionario
+            {
+                Nome = faker.Name.FullName(),
+                Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                Senioridade = Senioridade.Junior,
+                Salario = faker.Random.Double(3200, 5500)
+            }
+            };
+
+            yield return new object[]
+            {
+            new Funcionario
+            {
+                Nome = faker.Name.FullName(),
+Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                Senioridade = Senioridade.Pleno,
+                Salario = faker.Random.Double(5500, 8000)
+            }
+            };
+
+            yield return new object[]
+            {
+            new Funcionario
+            {
+                Nome = faker.Name.FullName(),
+                Nascimento = faker.Date.Between(DateTime.Now.AddDays(-21), DateTime.Now.AddDays(-50)),
+                Senioridade = Senioridade.Senior,
+                Salario = faker.Random.Double(8000, 20000)
+            }
+            };
+
+
+        }
     }
 }
